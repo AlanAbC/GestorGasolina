1b9221fcd9f0f68c785f0d56a07e6f5c4445bbe1
Ricardo Antonio Pizano Pérez <rikymon2@hotmail.com>
2017-04-22 22:45:22 -0500

    Se le dió funcionalidad a los menus de todos los activities a ecepcion de acerca de

--- app/src/main/java/com/claresti/gg/gestorgasolina/AcercaDe.java
+++ app/src/main/java/com/claresti/gg/gestorgasolina/AcercaDe.java
@@ -3,11 +3,11 @@ package com.claresti.gg.gestorgasolina;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class header_menu extends AppCompatActivity {
+public class AcercaDe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_header_menu);
+        setContentView(R.layout.activity_acerca_de);
     }
 }
--- app/src/main/java/com/claresti/gg/gestorgasolina/AgregarRegistro.java
+++ app/src/main/java/com/claresti/gg/gestorgasolina/AgregarRegistro.java
@@ -1,14 +1,90 @@
 package com.claresti.gg.gestorgasolina;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgregarRegistro extends AppCompatActivity {
+
+    //Menu, Declaracion de variables
+    private DrawerLayout drawerLayout;
+    final List<MenuItem> items = new ArrayList<>();
+    private Menu menu;
+    private ImageView btnMenu;
+    private NavigationView nav;
+    //Fin menu, declaracion de variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_agregar_registro);
+        //Cambiar el color en la barra de notificaciones (Solo funciona de lollipop hacia arriba)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.agregar_registro));
+        }
+        //Fin cambio de color de barra de notificaciones
+        //Menu, Inicia las variables del menu y llama la funcion encargada de su manipulacion
+        drawerLayout = (DrawerLayout) findViewById(R.id.dLayout);
+        nav = (NavigationView)findViewById(R.id.navigation);
+        menu = nav.getMenu();
+        menuNav();
+        // Fin menu
+    }
+
+    private void menuNav(){
+        for(int i = 0; i < menu.size(); i++){
+            items.add(menu.getItem(i));
+        }
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                int pos = items.indexOf(item);
+                if(pos == 0){
+                    finish();
+                }else if(pos == 1){
+                    Intent i = new Intent(AgregarRegistro.this, Estadisticas.class);
+                    startActivity(i);
+                    finish();
+                }else if(pos == 2){
+
+                }else if(pos == 3) {
+                    Intent i = new Intent(AgregarRegistro.this, Precios.class);
+                    startActivity(i);
+                    finish();
+                }else if(pos == 4){
+                    Intent i = new Intent(AgregarRegistro.this, AcercaDe.class);
+                    startActivity(i);
+                    finish();
+                }
+                drawerLayout.closeDrawer(nav);
+                item.setChecked(false);
+                return false;
+            }
+        });
+        btnMenu = (ImageView)findViewById(R.id.Btnmenu);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(nav);
+            }
+        });
     }
 }
