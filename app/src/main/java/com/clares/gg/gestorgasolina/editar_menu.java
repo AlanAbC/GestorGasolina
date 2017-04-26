ef6a07616f16b4642243ba9e14143b5c38bbb273
AlanAbC <alanjiraya77@gmail.com>
2017-04-26 07:28:13 -0500

    Se agrego el estilo al menu y el activity para editarlo

---
+++ app/src/main/java/com/clares/gg/gestorgasolina/editar_menu.java
@@ -0,0 +1,14 @@
+package com.clares.gg.gestorgasolina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editar_menu extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_menu);
+    }
+}
